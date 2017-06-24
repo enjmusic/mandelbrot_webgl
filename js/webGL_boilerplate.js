5e490ccad869ef36ccb6857dc4f92ee8410a330f
Ian Hoffman <ijh6@cornell.edu>
2017-06-24 00:57:54 -0700

    First commit

---
+++ js/webGL_boilerplate.js
@@ -0,0 +1,78 @@
+function initializeWebGL(canvasName) {
+    var canvas = $("#" + canvasName);
+    // Getting WebGL context the right way
+    var gl = null;
+    try {
+        gl = canvas[0].getContext("experimental-webgl");
+        if (!gl) {
+            gl = canvas[0].getContext("webgl");
+        }
+    } catch (error) {
+        // NO-OP
+    }
+    if (!gl) {
+        alert("Could not get WebGL context!");
+        throw new Error("Could not get WebGL context!");
+    }
+    return gl;
+}
+
+function createShader(gl, shaderScriptId) {
+    var shaderScript = $("#" + shaderScriptId);
+    var shaderSource = shaderScript[0].text;
+    var shaderType = null;
+    if (shaderScript[0].type == "x-shader/x-vertex") {
+        shaderType = gl.VERTEX_SHADER;
+    } else if (shaderScript[0].type == "x-shader/x-fragment") {
+        shaderType = gl.FRAGMENT_SHADER;
+    } else {
+        throw new Error("Invalid shader type: " + shaderScript[0].type)
+    }
+    var shader = gl.createShader(shaderType);
+    gl.shaderSource(shader, shaderSource);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        var infoLog = gl.getShaderInfoLog(shader);
+        gl.deleteShader(shader);
+        throw new Error("An error occurred compiling the shader: " + infoLog);
+    } else {
+        return shader;
+    }
+}
+
+function createGlslProgram(gl, vertexShaderId, fragmentShaderId) {
+    var program = gl.createProgram();
+    gl.attachShader(program, createShader(gl, vertexShaderId));
+    gl.attachShader(program, createShader(gl, fragmentShaderId));
+    gl.linkProgram(program);
+    gl.validateProgram(program);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        var infoLog = gl.getProgramInfoLog(program);
+        gl.deleteProgram(program);
+        throw new Error("An error occurred linking the program: " + infoLog);
+    } else {
+        return program;
+    }
+}
+
+function createShape(gl, vertexData, indexData) {
+    var shape = {};
+
+    var vertexArray = new Float32Array(vertexData);
+    var vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertexArray, gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    var indexArray = new Uint16Array(indexData);
+    var indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indexArray, gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+    shape.vertexBuffer = vertexBuffer;
+    shape.indexBuffer = indexBuffer;
+    shape.size = indexData.length;
+
+    return shape;
+}
